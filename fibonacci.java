d94d38dd00fce848bd9f69c380359c94453283d8
Sathish <pagdalasathish@gmail.com>
2024-11-30 09:05:51 +0530

    file is updates

--- fibonacci.java
+++ fibonacci.java
@@ -1,17 +1,32 @@
 package ProgrammingClass;
 import java.util.Scanner;
+
 public class fibonacci {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms in the Fibonacci series: ");
+        int num = sc.nextInt();
         isfibonacciseries(num);
     }
-    static void isfibonacciseries(int num){
-        int f1=0,f2=1,f3=0;
-        System.out.print(f1+","+f1",");
-        for(int i=1;i<=num-2;i++){
-        f3=f1+f2;
+
+    static void isfibonacciseries(int num) {
+        int f1 = 0, f2 = 1, f3;
+        
+        if (num >= 1) {
+            System.out.print(f1);
         }
+        if (num >= 2) {
+            System.out.print("," + f2);
+        }
+
+        for (int i = 3; i <= num; i++) {
+            f3 = f1 + f2;
+            System.out.print("," + f3);
+            f1 = f2;
+            f2 = f3;
+        }
+        System.out.println(); // New line after series
     }
 }
+
+
