ed496b33191cc9ff17e97055d5a265268b3a8b15
Sathish <pagdalasathish@gmail.com>
2024-12-08 16:13:54 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,11 +1,6 @@
-package ProgrammingClass;
-import java.util.Scanner;
-
 public class fibonacci {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of terms in the Fibonacci series: ");
-        int num = sc.nextInt();
+        int num = ( 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377);
         isfibonacciseries(num);
     }
 
